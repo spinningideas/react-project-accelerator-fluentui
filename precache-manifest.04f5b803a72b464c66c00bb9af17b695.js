e289fe1d74515067eab998eb6c7e417e3ab3bbef
spinningideas <scott@spinningideas.com>
2020-05-30 15:03:45 -0600

    Updates

---
+++ precache-manifest.04f5b803a72b464c66c00bb9af17b695.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a36b4d38fd2ea2c8276445d0c1d12d",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/index.html"
+  },
+  {
+    "revision": "19f418ea0fb925c88bd0",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/css/main.55e41eb6.chunk.css"
+  },
+  {
+    "revision": "a13a5cad8e36d6569319",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/2.4a4ddbad.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/2.4a4ddbad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9657a1a7effd39de7a3c",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/3.b55b9b9e.chunk.js"
+  },
+  {
+    "revision": "47a3a6708ba4f0a51f7c",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/4.f248b4df.chunk.js"
+  },
+  {
+    "revision": "3cdecfd5e42df6624e71",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/5.ad0aa10f.chunk.js"
+  },
+  {
+    "revision": "cdf2cb63bcd49ad7e319",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/6.e8e98a30.chunk.js"
+  },
+  {
+    "revision": "19f418ea0fb925c88bd0",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/main.85bc909f.chunk.js"
+  },
+  {
+    "revision": "dc29105bdd086374f7ee",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/runtime-main.4480d2ee.js"
+  }
+]);
