97abc5a92deecfad864612419a068e398a812d4a
spinningideas <scott@spinningideas.com>
2020-05-30 21:17:28 -0600

    Updates

---
+++ precache-manifest.4f68b391145a1fbc6a696c0f123b2e73.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cf8f2b02fd0d464a07ae7440d3fc88",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/index.html"
+  },
+  {
+    "revision": "2570bcf29f796ee667e3",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/css/main.91fdd53e.chunk.css"
+  },
+  {
+    "revision": "ba9120ef87f9e1547ba0",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/2.0cbc8bb0.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/2.0cbc8bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abb743ac2be9ed9524a",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/3.04d42488.chunk.js"
+  },
+  {
+    "revision": "99b1a4b3081d3983e071",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/4.53845694.chunk.js"
+  },
+  {
+    "revision": "10edbcd53db8ce4584f4",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/5.20b3141b.chunk.js"
+  },
+  {
+    "revision": "59c36b6296ba7e2e26dc",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/6.be390cd7.chunk.js"
+  },
+  {
+    "revision": "2570bcf29f796ee667e3",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/main.e710236b.chunk.js"
+  },
+  {
+    "revision": "2e9c5cc49014a27d3eff",
+    "url": "https://spinningideas.github.io/react-project-accelerator-fluentui/static/js/runtime-main.ced7bf69.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://spinningideas.github.io/react-project-accelerator-fluentui/precache-manifest.04f5b803a72b464c66c00bb9af17b695.js"
+  "https://spinningideas.github.io/react-project-accelerator-fluentui/precache-manifest.4f68b391145a1fbc6a696c0f123b2e73.js"
 );
 
 self.addEventListener('message', (event) => {
